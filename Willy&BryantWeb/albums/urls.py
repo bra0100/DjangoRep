d633ef1a0beaf0e56226309fb8f69befdb285405
Bryant <bryantgh1984@outlook.es>
2025-09-22 18:17:08 -0400

    Update urls.py

--- Willy&BryantWeb/albums/urls.py
+++ Willy&BryantWeb/albums/urls.py
@@ -3,18 +3,17 @@ from django.contrib.auth import views as auth_views
 from .views import HomeView, AlbumListView, AlbumUpdateView, AlbumDeleteView, register, upload_album,  custom_login, custom_logout
 
 urlpatterns = [
-    # Página de inicio REAL
+
     path('', HomeView.as_view(), name='home'),
 
-    # Lista completa de álbumes
     path('albums/', AlbumListView.as_view(), name='album_list'),
 
-    # Other CRUD operations
+
     path('upload/', upload_album, name='upload_album'),
     path('album/<int:pk>/edit/', AlbumUpdateView.as_view(), name='edit_album'),
     path('album/<int:pk>/delete/', AlbumDeleteView.as_view(), name='delete_album'),
 
-    # Authentication
+
     path('login/', custom_login, name='login'),
     path('register/', register, name='register'),
     path('logout/', custom_logout, name='logout'),
